dd6e5bb804a4cf73d7d59e98a2eec90c93f98bef
Pravin <pravinkumar2108@gmail.com>
2023-06-26 20:11:45 +0530

    Super().fun() in Python to solve overriding

--- super_awesome.py
+++ super_awesome.py
@@ -1,12 +1,13 @@
+# So in Python we avoid Function Overriding using " Super().function_def() "
 class Dog:
     def __init__(self):
         print("Woof Woof")
     def pee(self):
         print("I Will Pee")
 
-    
 class Puppy(Dog):
     def pee(self):
+        super().pee()       # Whenever we use Superclass 
         print("Go to the park")
 
 
